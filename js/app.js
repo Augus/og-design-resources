8c5ca4e09541ac8eb1d5b659b61e4adbca0b2ad3
Augus <augus790302@gmail.com>
2014-07-16 22:50:58 +0800

    暫時隱藏quick menu, 移除jquery, angularjs

--- js/app.js
+++ js/app.js
@@ -1,50 +1,50 @@
-var app = angular.module("resourcesApp", []);
+// var app = angular.module("resourcesApp", []);
 
-app.controller('RootController', function($scope, $location){
+// app.controller('RootController', function($scope, $location){
 
-    $scope.items = [];
+//     $scope.items = [];
 
-    $(document).ready(function () {
+//     $(document).ready(function () {
 
-        $(window).on("scroll", function () {
-            var scrollTop = $(this).scrollTop(),
-                distance = 5000,
-                i;
+//         $(window).on("scroll", function () {
+//             var scrollTop = $(this).scrollTop(),
+//                 distance = 5000,
+//                 i;
 
-            $(".menu-item").each(function () {
-                $(this).removeClass("active");
-            });
+//             $(".menu-item").each(function () {
+//                 $(this).removeClass("active");
+//             });
 
-            $(".feature-title").each(function (index) {
-                var d = Math.abs($(this).offset().top - scrollTop);
-                if (d < distance) {
-                    distance = d;
-                    i = index;
-                }
-            })
+//             $(".feature-title").each(function (index) {
+//                 var d = Math.abs($(this).offset().top - scrollTop);
+//                 if (d < distance) {
+//                     distance = d;
+//                     i = index;
+//                 }
+//             })
 
-            $(".menu-item").eq(i).addClass("active");
+//             $(".menu-item").eq(i).addClass("active");
 
-        });
+//         });
 
-        $scope.$apply(function () {
-            $(".feature-title").each(function () {
-                $scope.items.push($(this).text());
-            })
-        });
+//         $scope.$apply(function () {
+//             $(".feature-title").each(function () {
+//                 $scope.items.push($(this).text());
+//             })
+//         });
 
-    });
+//     });
 
 
-    $scope.scrollTo = function (index) {
-        var scrollTop = $(".feature-title").eq(index).offset().top;
-        $.scrollTo(scrollTop - 40, 650, {easing:'swing', axis:'y'});
-        // $(window).css("scrollTop", scrollTop - 40);
-    };
+//     $scope.scrollTo = function (index) {
+//         var scrollTop = $(".feature-title").eq(index).offset().top;
+//         $.scrollTo(scrollTop - 40, 650, {easing:'swing', axis:'y'});
+//         // $(window).css("scrollTop", scrollTop - 40);
+//     };
 
-	$scope.navClass = function (page) {
-    	var currentRoute = $location.path().substring(1) || 'home';
-	    return page === currentRoute;
-	};
-});
+// 	$scope.navClass = function (page) {
+//     	var currentRoute = $location.path().substring(1) || 'home';
+// 	    return page === currentRoute;
+// 	};
+// });
 
